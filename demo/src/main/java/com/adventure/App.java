d309000bf5b8f34c045dd4c1e9b2d789b1fcb9ca
berryes <therealkispetofi@gmail.com>
2023-01-26 09:37:40 +0100

    0.0.0.2

--- demo/src/main/java/com/adventure/App.java
+++ demo/src/main/java/com/adventure/App.java
@@ -1,13 +1,23 @@
-package demo.src.main.java.com.adventure;
+package com.adventure;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        try {
+            // Enter data using BufferReader
+        BufferedReader reader = new BufferedReader( new InputStreamReader(System.in) );
+
+        System.out.println("Enter username:");
+
+        String player = reader.readLine();
+        System.out.println( player );
+        
+        } catch (Exception e) {
+            System.out.println(e);   
+        }
+        
     }
 }
