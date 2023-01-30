0829602f2c0f0e0f84e3c460ccd304260ba6d530
berryes <therealkispetofi@gmail.com>
2023-01-30 21:16:57 +0100

    some stuff

---
+++ characters.md
@@ -0,0 +1,55 @@
+```
+         __/)     (\__
+      ,-'~~(   _   )~~`-.
+     /      \/'_`\/      \
+    |       /_(_)_\       |
+    |     _(/(\_/)\)_     |
+    |    / // \ / \\ \    |
+     \  | ``  / \ ''  |  /
+      \  )   /   \   (  /
+       )/   /     \   \(
+       '    `-`-'-'    `
+            The angel
+
+        attack: 6
+        health: 1.5
+        protection: 3
+
+"The angel is out of this world, it controls the blood of the enemies at a cost of protection"
+```
+
+
+```
+            .-=--.                
+            .' .--. '.              
+        :  : .-.'. :    _ _         
+        :  : : .': :   (q)q)     
+        :  '. '-' .'   ///      
+        _'.__'--=' '-.///       
+        .-'               /         
+        '---..____...---''    
+            Sephtis
+
+        attack: 2
+        health: 2.5
+        protection: 100 (5)
+
+"Sephtis is an eternal snail. He hides in his shell from incoming attacks, and can be only defeated by cutting his eyes off..."
+```
+
+```
+                     __       __
+                     '.'--.--'.-'
+       .,_------.___,   \' r'
+       ', '-._O      '-' .'
+        '.    '-'Y \._  /
+          '--;____'--.'-,
+           /..'       '''
+               Whale
+
+            attack: 3
+            health: 6
+            protection: 3
+            "Just a whale"
+```                               
+
--- demo/src/main/java/com/adventure/App.java
+++ demo/src/main/java/com/adventure/App.java
@@ -1,23 +1,53 @@
 package com.adventure;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.ArrayList;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-        try {
-            // Enter data using BufferReader
-        BufferedReader reader = new BufferedReader( new InputStreamReader(System.in) );
+import javax.swing.JFrame;
+import javax.swing.JTextField;
 
-        System.out.println("Enter username:");
+public class App {
 
-        String player = reader.readLine();
-        System.out.println( player );
+    private String getUserInputUntilEnterIsPressed(){
         
-        } catch (Exception e) {
-            System.out.println(e);   
-        }
+        String input = "";
+
+
+        return "asd";
+    }
+
+    private boolean listen = false;
+
+    public static void main(String args[]) {
+
+        // keyboard listener
+        JFrame frame = new JFrame("Game listener");
+        Container contentPane = frame.getContentPane();
+        KeyListener listener = new KeyListener() {
+            @Override
+            public void keyPressed(KeyEvent event) {
+                // System.out.println( event.getKeyChar() );
+            }
+            @Override
+            public void keyReleased(KeyEvent event) {
+                // System.out.println( event.getKeyChar() );
+            }
+
+            @Override
+            public void keyTyped(KeyEvent event) {
+              System.out.println( event.getKeyChar());
+            }
+        
+        };
+        // idk text field in the shit
+        JTextField textField = new JTextField(); // some textfield bs
+        textField.addKeyListener(listener); // adds key listener
+        contentPane.add(textField, BorderLayout.NORTH); // idk
+        frame.pack(); // idk wtf this does
+        frame.setVisible(true); // maybe sets it visible? idk
+
         
     }
 }
