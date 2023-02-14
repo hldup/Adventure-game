2938fb7dfeef3acfc34e3de0213256f5c3fb65e9
berryes <therealkispetofi@gmail.com>
2023-02-14 03:51:55 +0100

    0.0.0.3
    Working keylogger, now i can actually begin makin' this game xd
    
    Total lines & code: 202 | 62

---
+++ src/main.rs
@@ -0,0 +1,83 @@
+use std::process::exit;
+
+use rdev::{listen, Event};
+
+
+#[derive(Debug)]
+enum  Key {
+    Delete,
+    Enter,
+    Escape,
+    Char{
+        value: String, // for example: a p v x | on the keyboard
+    },
+    Void, // a way to ignore static typing
+}
+fn FilterInputStreamForKeys(event:Event) -> Key{
+
+    // Filtering out event that are only ButtonPresses
+    match event.event_type {
+
+        ButtonPress =>{
+
+            match event.name {
+                Some(key) =>{ // filtering out Some (some -> String)
+
+                    match key.as_str() {
+                            "\r" => return Key::Enter, 
+                            r"\u{8}" => return Key::Delete,
+                            r"\u{1b}" => return Key::Escape,
+                            &_ => return  Key::Char { value: key }, // returning void as a workaround
+                        };
+                }
+                None => return Key::Void
+            }
+        }
+
+        // If its not a ButtonPress do nada
+        _ => todo!("asd")
+    }
+}
+
+
+fn main(){
+
+let mut username: String = String::new();
+
+// listener
+listen(
+    // moving shit
+move |event:Event | -> () {
+    let key:Key = FilterInputStreamForKeys(event);
+    // Key::Char{ value: String }
+    match key {
+        Key::Char { value: n } =>{
+            username.push_str(n.as_str())
+        },
+
+        Key::Delete => {
+            username.remove(username.len() +1 ); 
+            return ()
+        },
+
+        Key::Escape => {
+            exit(0)
+        },
+        // ew 
+        Key::Void =>{
+            return ()
+        },
+
+        Key::Enter =>{
+          // go to next stage
+            return ()
+        }
+
+    }
+
+}).expect("asd");
+
+
+
+
+}
